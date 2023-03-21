4be2537db0c4e637f9dbea90f13cae497a9e97ec
devignasrocys <devignasrocys@gmail.com>
2023-03-21 13:32:39 +0200

    fixed product review so only those customers who bought it can leave a review

--- e_commerce_shop/core/views.py
+++ e_commerce_shop/core/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from . import models
 from . import forms
+from payment import models as payment_models
 
 
 # Create your views here.
@@ -26,13 +27,17 @@ def categories(request):
 def list_category(request, category_slug=None):
     category = get_object_or_404(models.Category, slug=category_slug)
     products = models.Product.objects.filter(category=category)
-    print(products)
     return render(request, 'core/list-category.html', {'category': category, 'products': products})
 
 
 def product_info(request, slug):
     product = get_object_or_404(models.Product, slug=slug)
     reviews = models.ProductReview.objects.filter(product=product)
+    products_bought_by_user = payment_models.OrderItem.objects.filter(user=request.user, product=product)
+    if products_bought_by_user:
+        product_already_bought = True
+    else:
+        product_already_bought = False
     if request.method == 'POST':
         form = forms.ProductReviewForm(request.POST)
         if form.is_valid():
@@ -41,7 +46,6 @@ def product_info(request, slug):
             review.product = product
             review.save()
             return redirect('product-info', slug=slug)
-    context = {'product': product, 'form': forms.ProductReviewForm(), 'reviews': reviews}
-    return render(request, 'core/product-info.html', context=context)
+    return render(request, 'core/product-info.html', {'product': product, 'form': forms.ProductReviewForm(), 'reviews': reviews, 'product_already_bought': product_already_bought})
 
 
